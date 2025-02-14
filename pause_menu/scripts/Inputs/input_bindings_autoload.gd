4c804ea3a3785e4ab6afc639367efcd289239a9a
Nathan Ngo <nathanj238@gmail.com>
2025-02-15 09:40:57 +1100

    Removed pause_menu from directory root

--- pause_menu/scripts/Inputs/input_bindings_autoload.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-class_name InputBindingsAutoload
-
-##a dictionary with the names of the inputs using the Input Action Prompt Icon Font plugin
-@export var button_prompt_Dictionary : Dictionary
