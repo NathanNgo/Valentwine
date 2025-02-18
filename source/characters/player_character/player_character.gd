be5eec63310d2ea3d5decf5165ed22efa6dcd719
Nathan Ngo <nathanj238@gmail.com>
2025-02-18 23:52:09 +1100

    Cleaned up stun and interaction code

--- source/characters/player_character/player_character.gd
+++ source/characters/player_character/player_character.gd
@@ -1,8 +1,7 @@
 class_name PlayerCharacter extends CharacterBody2D
 
 signal player_damaged(damage_amount: float)
-signal stun_pressed
-signal interrupted
+# signal stun_pressed
 
 enum Player { FIRST, SECOND }
 enum Controls { LEFT, RIGHT, UP, DOWN, INTERACT }
@@ -19,15 +18,15 @@ static var player_group := "player_objects"
 @export var stagger_amount := 150
 @export var attack_range := 150
 
-@export var stun_container : Node2D
-@export var _left_prompt : Sprite2D
-@export var _right_prompt : Sprite2D
+@export var stun_container: Node2D
+@export var _left_prompt: Sprite2D
+@export var _right_prompt: Sprite2D
 @export var grace_timer: Timer
 @export var animation_player: AnimationPlayer
 
 var state := State.IDLE
 var current_stun_prompt := Controls.LEFT
-var current_interacting_object : Node2D
+var current_interacting_object: Node2D
 var punches_in_a_row: int = 0
 var punches_in_combo: int = 3
 var control_schemes := {
@@ -56,6 +55,7 @@ func _ready() -> void:
 	_player_one_sprite.hide()
 	_player_two_sprite.hide()
 	stun_container.hide()
+
 	if player_type == Player.FIRST:
 		_player_one_sprite.show()
 	elif player_type == Player.SECOND:
@@ -77,17 +77,23 @@ func _physics_process(_delta: float) -> void:
 
 	ray_cast_2d.target_position = direction * attack_range
 	stun_container.hide()
+
 	match state:
 		State.BLOCKED:
 			return
+
 		State.STUNNED:
 			stun_container.show()
-			handle_stun(Controls.UP)
+			stun(Controls.UP)
+
 			if Input.is_action_just_pressed(selected_scheme[Controls.LEFT]):
-				handle_stun(Controls.LEFT)
+				stun(Controls.LEFT)
+
 			if Input.is_action_just_pressed(selected_scheme[Controls.RIGHT]):
-				handle_stun(Controls.RIGHT)
+				stun(Controls.RIGHT)
+
 			return
+
 		State.IDLE:
 			idle(direction)
 
@@ -99,25 +105,24 @@ func idle(direction: Vector2) -> void:
 	if not ray_cast_2d.is_colliding():
 		return
 
-	var attack_target: Node2D = ray_cast_2d.get_collider()
+	var target: Node2D = ray_cast_2d.get_collider()
 
-	if not attack_target:
+	if not target:
 		return
 
-	if attack_target.is_in_group(Enemy.enemy_group):
-		attack(attack_target, direction)
+	if target.is_in_group(Enemy.enemy_group):
+		attack(target, direction)
 		return
 
-	if attack_target.is_in_group("interactable"):
-		current_interacting_object = attack_target
-		current_interacting_object.finished_interaction.connect(resolve_interaction)
-		attack_target.interact(self)
+	if target.is_in_group(Interactable.interactable_group):
+		current_interacting_object = target
+		current_interacting_object.open_interaction(self)
 
 
 func damage(damage_amount: float, attack_direction: Vector2 = Vector2.ZERO) -> void:
 	player_damaged.emit(damage_amount)
 	global_position = global_position + attack_direction * stagger_amount
-	interrupt_interaction()
+	close_interaction()
 
 
 func attack(attack_target: Node2D, attack_direction: Vector2 = Vector2.ZERO) -> void:
@@ -148,33 +153,31 @@ func finish_attack() -> void:
 	grace_timer.start(grace_time_between_punches)
 
 
-func resolve_interaction() -> void:
-	current_interacting_object.finished_interaction.disconnect(resolve_interaction)
-	current_interacting_object = null
-	state = State.IDLE
+func close_interaction() -> void:
+	if not current_interacting_object:
+		return
 
-
-func interrupt_interaction() -> void:
-	interrupted.emit()
-	if current_interacting_object:
-		current_interacting_object.finished_interaction.disconnect(resolve_interaction)
+	current_interacting_object.close_interaction()
 	state = State.IDLE
 	current_interacting_object = null
 
 
-func handle_stun(which_button_pressed : Controls) -> void:
+func stun(button_pressed: Controls) -> void:
 	if current_stun_prompt == Controls.LEFT:
 		_left_prompt.show()
 		_right_prompt.hide()
+
 	if current_stun_prompt == Controls.RIGHT:
 		_left_prompt.hide()
 		_right_prompt.show()
-	if which_button_pressed == current_stun_prompt and current_stun_prompt == Controls.LEFT:
+
+	if button_pressed == current_stun_prompt and current_stun_prompt == Controls.LEFT:
 		current_stun_prompt = Controls.RIGHT
-		stun_pressed.emit()
-	if which_button_pressed == current_stun_prompt and current_stun_prompt == Controls.RIGHT:
+		current_interacting_object.interact()
+
+	if button_pressed == current_stun_prompt and current_stun_prompt == Controls.RIGHT:
 		current_stun_prompt = Controls.LEFT
-		stun_pressed.emit()
+		current_interacting_object.interact()
 
 
 func _on_grace_timer_timeout() -> void:
---
+++ source/interactables/interactable.gd
@@ -0,0 +1,37 @@
+class_name Interactable extends StaticBody2D
+
+signal interaction_finished
+
+static var interactable_group := "interactable"
+
+@export var number_of_presses_required: int = 5
+
+var _presses: int = 0
+var _player: PlayerCharacter
+
+
+func open_interaction(player: PlayerCharacter) -> void:
+	if _player != null:
+		# If the player is not null, we're already interacting.
+		return
+
+	_player = player
+	player.state = PlayerCharacter.State.STUNNED
+
+
+func close_interaction() -> void:
+	_presses = 0
+	_player = null
+	interaction_finished.emit()
+
+
+func interact() -> void:
+	_presses += 1
+
+	if _presses != number_of_presses_required:
+		return
+
+	if not _player.has_method("close_interaction"):
+		return
+
+	_player.close_interaction()
