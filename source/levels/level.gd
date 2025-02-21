0d7b6c732c9d7b31f451b45012ca624d4b0aa5e8
Nathan Ngo <nathanj238@gmail.com>
2025-02-21 22:59:18 +1100

    Added properties to base level

---
+++ source/levels/level.gd
@@ -0,0 +1,8 @@
+class_name Level extends Node2D
+
+@export var tile_maps: Node2D
+@export var enemies_container: Node2D
+@export var obstacles_container: Node2D
+@export var interactables_container: Node2D
+
+
