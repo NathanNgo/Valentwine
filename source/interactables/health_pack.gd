dd2db7f4b714ef34c7c626a8fc139d3c04264757
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-02-23 17:32:32 +0100

    the version we submitted to the jam

--- source/interactables/health_pack.gd
+++ source/interactables/health_pack.gd
@@ -1,12 +1,20 @@
 extends Area2D
 
 @export var health : float = 50
+@export var sound : FmodEventEmitter2D
+
+var disabled : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(on_body_entered)
+	sound.stopped.connect(queue_free)
 
 
 func on_body_entered(body : Node2D) -> void:
+	if disabled:
+		return
 	if body.is_in_group(PlayerCharacter.player_group):
+		hide()
+		disabled = true
 		body.damage(-health)
-		queue_free()
+		sound.play()
--- source/obstacles/obstacle.gd
+++ source/obstacles/obstacle.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var damage_amount := 10
-
+@export var sound : FmodEventEmitter2D
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -10,3 +10,4 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player_objects"):
 		body.damage(damage_amount)
+		sound.play()
